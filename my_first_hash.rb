443ee5f618a3624ec8aebb09fc8215ebb067e916
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 01:06:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+  shipping_manifest ["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -37,11 +37,9 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
-
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-
   # return the shipping_manifest hash below
-  return shipping_manifest ["oil paintings"]
+  return shipping_manifest
 end
